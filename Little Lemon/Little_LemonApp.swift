ae86c4c66241d04d316fc98059ca9de5e8252f3e
Randy McKown <randymckown@hotmail.com>
2025-01-18 12:48:25 -0600

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Randy McKown on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
